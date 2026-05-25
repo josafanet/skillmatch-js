88ba6a7d7c15d05d030ecd96d7abe347367034eb
Josafá <josafart.net@gmail.com>
2026-05-25 09:44:21 -0300

    feat: adiciona perfil do candidato

---
+++ skillmatch.js
@@ -0,0 +1,17 @@
+// SkillMatch JS - Simulador de Compatibilidade com Vagas Front-End Junior
+
+const candidato = {
+  nome: "Josafa",
+  area: "Front-End",
+  habilidades: [
+    "JavaScript",
+    "GitHub",
+    "Logica de Programacao",
+    "Kanban",
+  ],
+  experienciaMeses: 3,
+  possuiPortfolio: true,
+};
+
+console.log("Candidato cadastrado:");
+console.log(candidato);
